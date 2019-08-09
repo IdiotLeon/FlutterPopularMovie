1eebe5c53632dc3244286871e7a0fa9222182cce
Yang Lu (Leon) <yanglyu.leon.7@gmail.com>
2019-08-08 19:23:05 -0700

    fea: Getting popular movies from the API for the very 1st time

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-void main() => runApp(MainApp());
+const String URL_BASE = "https://api.themoviedb.org";
+const String URL_POPULAR_MOVIE = "/3/movie/popular";
+// const String API_KEY = "?api_key=${DotEnv().env['API_KEY']}";
+const String API_KEY = "?api_key=";
+
+void main() async {
+  await DotEnv().load('.env');
+  runApp(MainApp());
+}
 
 class MainApp extends StatelessWidget {
   @override
@@ -25,7 +35,7 @@ class MainAppPage extends StatefulWidget {
 }
 
 class _MainAppPageState extends State<MainAppPage> {
-  List widgets = [];
+  Map<String, dynamic> movies = new Map();
 
   @override
   void initState() {
@@ -40,24 +50,37 @@ class _MainAppPageState extends State<MainAppPage> {
           title: Text("Popular Movie"),
         ),
         body: ListView.builder(
-          itemCount: widgets.length,
+          itemCount: movies['results'].length,
           itemBuilder: (BuildContext context, int position) {
             return getRow(position);
           },
-        ));
+        )
+        // body: GridView.builder(
+        //   itemBuilder: ,
+        //   gridDelegate: ,
+        // ),
+        );
   }
 
   Widget getRow(int i) {
-    return Padding(
-        padding: EdgeInsets.all(10.0),
-        child: Text("Row ${widgets[i]["title"]}"));
+    return Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+      Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Text("Row ${movies['results'][i]["title"]}")),
+      Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Text("Row ${movies['results'][i]["overview"]}")),
+    ]);
   }
 
   loadData() async {
-    String dataUrl = "https://jsonplaceholder.typicode.com/posts";
+    String dataUrl = URL_BASE +
+        URL_POPULAR_MOVIE +
+        API_KEY +
+        DotEnv().env['API_KEY_MOVIE_DB'];
     http.Response response = await http.get(dataUrl);
     setState(() {
-      widgets = json.decode(response.body);
+      movies = json.decode(response.body);
     });
   }
 }
