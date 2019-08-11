9b0a472c922cf8a8dd5d5a3285b8a480dd35e42d
Leon <yanglyu.leon.7@gmail.com>
2019-08-11 16:38:50 -0700

    fea: Acquiring the location successfully

--- lib/map.dart
+++ lib/map.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class MapPage extends StatefulWidget {
   static const routeName = "/mapPage";
@@ -16,6 +18,9 @@ class MapPageState extends State<MapPage> {
 
   static const LatLng center = const LatLng(45.521563, -122.677433);
 
+  var location = new Location();
+  LocationData userLocation;
+
   MapType currentMapType = MapType.normal;
 
   void onMapTypeButtonPressed() {
@@ -27,6 +32,17 @@ class MapPageState extends State<MapPage> {
 
   void onMapCreated(GoogleMapController controller) {
     this.controller.complete(controller);
+
+    getLocation().then((onValue) {
+      userLocation = onValue;
+      print('getLocation(): ' + userLocation.latitude.toString());
+      print('getLocation(): ' + userLocation.longitude.toString());
+    });
+
+    location.onLocationChanged().listen((onData) {
+      print(onData.latitude);
+      print(onData.longitude);
+    });
   }
 
   @override
@@ -57,5 +73,22 @@ class MapPageState extends State<MapPage> {
         ),
       ],
     );
+  }
+
+  Future<LocationData> getLocation() async {
+    LocationData currentLocation;
+    var location = new Location();
+
+    try {
+      currentLocation = await location.getLocation();
+    } on PlatformException catch (e) {
+      if (e.code == 'PERMISSION_DENIED') {
+        print('Permission denied');
+      }
+
+      currentLocation = null;
+    }
+
+    return currentLocation;
   }
 }
