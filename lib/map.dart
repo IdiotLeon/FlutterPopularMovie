1850bfe60279f0219714c04c2f8c50c08c5bb695
Yang Lu (Leon) <yanglyu.leon.7@gmail.com>
2019-08-11 13:35:38 -0700

    fea: Adding one FAB

--- lib/map.dart
+++ lib/map.dart
@@ -16,17 +16,46 @@ class MapPageState extends State<MapPage> {
 
   static const LatLng center = const LatLng(45.521563, -122.677433);
 
+  MapType currentMapType = MapType.normal;
+
+  void onMapTypeButtonPressed() {
+    setState(() {
+      currentMapType =
+          currentMapType == MapType.normal ? MapType.satellite : MapType.normal;
+    });
+  }
+
   void onMapCreated(GoogleMapController controller) {
     this.controller.complete(controller);
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: GoogleMap(
-        onMapCreated: this.onMapCreated,
-        initialCameraPosition: CameraPosition(target: center, zoom: 11.0),
-      ),
+    return Stack(
+      children: <Widget>[
+        GoogleMap(
+          onMapCreated: this.onMapCreated,
+          initialCameraPosition: CameraPosition(target: center, zoom: 11.0),
+          mapType: currentMapType,
+        ),
+        Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Align(
+            alignment: Alignment.topRight,
+            child: Column(
+              children: <Widget>[
+                FloatingActionButton(
+                  onPressed: onMapTypeButtonPressed,
+                  materialTapTargetSize: MaterialTapTargetSize.padded,
+                  backgroundColor: Colors.green,
+                  child: const Icon(Icons.map, size: 36.0),
+                ),
+                SizedBox(height: 16.0),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
