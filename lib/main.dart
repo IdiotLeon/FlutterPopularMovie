df5c9274fd130ec2a561b35c45124b86da177632
Yang Lu (Leon) <yanglyu.leon.7@gmail.com>
2019-08-10 18:59:07 -0700

    refactor: Putting the GridView into a Stateless Widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_popular_movie/models.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'detail.dart';
+import 'user_profile.dart';
 
 const String URL_BASE = "https://api.themoviedb.org";
 const String URL_POPULAR_MOVIE = "/3/movie/popular";
@@ -24,9 +27,82 @@ class MainApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MainAppPage(),
+      home: PopularMoviesHomePage(title: "Popular Movies"),
     );
   }
+}
+
+class PopularMoviesHomePage extends StatelessWidget {
+  final String title;
+
+  PopularMoviesHomePage({Key key, @required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text(title)),
+        drawer: Drawer(),
+        body: FutureBuilder<List<ResponseMovieResult>>(
+            future: fetchMovies(http.Client()),
+            builder: (context, snapshot) {
+              if (snapshot.hasError) print(snapshot.error);
+
+              return snapshot.hasData
+                  ? MoviesList(movies: snapshot.data)
+                  : Center(child: CircularProgressIndicator());
+            }));
+  }
+}
+
+class MoviesList extends StatelessWidget {
+  final List<ResponseMovieResult> movies;
+
+  MoviesList({Key key, this.movies}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return new GestureDetector(
+      child: new Card(
+        elevation: 5.0,
+        child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+          ),
+          itemCount: movies.length,
+          itemBuilder: (context, index) {
+            return Container(
+              alignment: Alignment.center,
+              child: CachedNetworkImage(
+                imageUrl: URL_IMAGE_BASE + movies[index].posterPath,
+                placeholder: (context, url) => CircularProgressIndicator(),
+                errorWidget: (context, url, error) => new Icon(Icons.error),
+                fit: BoxFit.fill,
+                fadeInCurve: Curves.easeIn,
+              ),
+              padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+            );
+          },
+        ),
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+      ),
+      onTap: () {
+        print("Hello World");
+      },
+    );
+  }
+}
+
+Future<List<ResponseMovieResult>> fetchMovies(http.Client client) async {
+  var urlPopularMovies =
+      URL_BASE + URL_POPULAR_MOVIE + API_KEY + DotEnv().env['API_KEY_MOVIE_DB'];
+  final response = await client.get(urlPopularMovies);
+  return parseResponse(response.body);
+}
+
+List<ResponseMovieResult> parseResponse(String responseBody) {
+  final parsed = Map<String, dynamic>.from(json.decode(responseBody));
+  return (new ResponsePopularMovie.fromJson(parsed)).results.toList();
 }
 
 class MainAppPage extends StatefulWidget {
@@ -50,6 +126,37 @@ class _MainAppPageState extends State<MainAppPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Popular Movie"),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: Text("Leon"),
+              accountEmail: Text("idiotleon"),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor:
+                    Theme.of(context).platform == TargetPlatform.iOS
+                        ? Colors.blue
+                        : Colors.white,
+                child: Text(
+                  "A",
+                  style: TextStyle(fontSize: 40.0),
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text("Item 1"),
+              trailing: Theme.of(context).platform == TargetPlatform.android
+                  ? Icon(Icons.arrow_forward)
+                  : Icon(Icons.arrow_forward_ios),
+              onTap: () {
+                Navigator.of(context).pop();
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (BuildContext context) => UserProfile()));
+              },
+            ),
+          ],
+        ),
       ),
       // body: ListView.builder(
       //   itemCount: movies['results'].length,
@@ -111,8 +218,11 @@ class _MainAppPageState extends State<MainAppPage> {
         API_KEY +
         DotEnv().env['API_KEY_MOVIE_DB'];
     http.Response response = await http.get(dataUrl);
+    final parsed = json.decode(response.body).cast<Map<String, dynamic>>();
+    var responsePopularMovie = parsed.map<ResponsePopularMovie>(
+        (json) => ResponsePopularMovie.fromJson(json));
     setState(() {
-      movies = json.decode(response.body)['results'];
+      movies = responsePopularMovie.results;
     });
   }
 }
